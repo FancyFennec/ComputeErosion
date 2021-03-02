3d05a9dfd041ca4fd151600ad16f9993bd38f3de
FancyFennec <jonasallemann@gmx.ch>
2021-03-02 21:33:37 +0100

    now the perlin noise shader is accessed in the editor

--- Assets/Editor/ErosionEditor.cs
+++ Assets/Editor/ErosionEditor.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class ErosionEditor : MonoBehaviour
+[CustomEditor(typeof(ErodingTerrain))]
+public class ErosionEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    ErodingTerrain terrain;
+    ComputeShader shader;
+
+    Renderer rend;
+
+    int kernelHandle;
+
+    private void OnEnable()
+	{
+		terrain = (ErodingTerrain)target;
+        shader = (ComputeShader)Resources.Load("PerlinNoiseComputeShader");
+        kernelHandle = shader.FindKernel("CSMain");
+
+        initialiseHeightMap();
+
+        rend = terrain.GetComponent<Renderer>();
+		rend.enabled = true;
+	}
+
+	private void OnSceneGUI()
     {
-        
+        shader.SetFloat("time", (float) EditorApplication.timeSinceStartup);
+
+        shader.Dispatch(kernelHandle, terrain.resolution / 8, terrain.resolution / 8, 1);
+
+        rend.material.SetTexture("Texture2D_f24a80a3f47f4c20844d82524f9db08d", terrain.heightMap);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        terrain.resolution = EditorGUILayout.IntField("Resolution ", terrain.resolution);
+
+        shader.SetInt("resolution", terrain.resolution);
+        initialiseHeightMap();
+    }
+
+    private void initialiseHeightMap()
+    {
+        terrain.heightMap = new RenderTexture(terrain.resolution, terrain.resolution, 32) { enableRandomWrite = true };
+        terrain.heightMap.format = RenderTextureFormat.RFloat;
+        terrain.heightMap.Create();
+
+        shader.SetTexture(kernelHandle, "heightMap", terrain.heightMap);
     }
 }
---
+++ Assets/Scripts/ErodingTerrain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErodingTerrain : MonoBehaviour
+{
+    public RenderTexture heightMap;
+    public int resolution = 1024;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
