74a7d133689d594ae8d968bdf6c1756e22f64fc2
FancyFennec <jonasallemann@gmx.ch>
2021-05-24 21:31:25 +0200

    solved most border problems

---
+++ Assets/Resources/helpers.h
@@ -0,0 +1,11 @@
+﻿bool isInside(uint3 id, int resolution);
+
+//bool isInside(int i, int j, uint3 id, int resolution)
+//{
+//	return id.x + i >= 0 && id.y + j >= 0 && id.x + i < resolution - 1 && id.y + j < resolution - 1;
+//}
+
+bool isInside(uint3 id, int resolution)
+{
+	return id.x > 0 && id.y > 0 && id.x < resolution - 1 && id.y < resolution - 1;
+}
